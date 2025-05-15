851b249a8bd6529b2fd1f1ad5f3189e96c0e296c
Ismailakram300 <ismailakram300@gmail.com>
2025-05-15 14:31:57 +0500

    update

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_practice/Screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -8,12 +10,27 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Home Page"),
         backgroundColor: Colors.green.shade100,
         centerTitle: true,
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.logout),
+            onPressed: (){
+              auth.signOut().then((value){
+                Navigator.push(context,MaterialPageRoute(builder:(context)=> SplashScreen()));
+              });
+            },
+          ),
+
+        ],
       ),
       body: Center(
         child: Column(
--- lib/Screens/splash_screen.dart
+++ lib/Screens/splash_screen.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_practice/Screens/home_page.dart';
 import 'package:firebase_practice/Screens/login_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -12,11 +15,21 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  FirebaseAuth auth = FirebaseAuth.instance;
+void Check(){
+  final user=auth.currentUser;
+  if(user!=null){
+    print(user);
+    Timer(Duration(seconds: 3), ()=>Navigator.push(context, MaterialPageRoute(builder: (context)=> HomePage()),), );
 
+  }else{
+    Timer(Duration(seconds: 3), ()=>Navigator.push(context, MaterialPageRoute(builder: (context)=> LoginScreen()),), );
 
+  }
+}
   void initState(){
     super.initState();
-    Timer(Duration(seconds: 3), ()=>Navigator.push(context, MaterialPageRoute(builder: (context)=> LoginScreen()),), );
+    Check();
   }
   @override
   Widget build(BuildContext context) {
