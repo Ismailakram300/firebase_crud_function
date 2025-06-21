062dd0c93a12976d18b68f16b9d9a677050cc456
Ismailakram300 <123332697+Ismailakram300@users.noreply.github.com>
2025-06-21 23:53:09 +0500

    Update home_page.dart

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -6,10 +6,7 @@ import 'package:firebase_practice/Screens/splash_screen.dart';
 import 'package:firebase_practice/uitils/tostmessae.dart';
 import 'package:firebase_remote_config/firebase_remote_config.dart';
 import 'package:flutter/material.dart';
-class HomePage extends StatefulWidget {
-
-  
-  
+class HomePage extends StatefulWidget {  
   const HomePage({super.key});
 
   @override
