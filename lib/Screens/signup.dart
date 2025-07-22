ced96226b1188e62d724137b148f6f1a42a8ea95
Ismailakram300 <123332697+Ismailakram300@users.noreply.github.com>
2025-07-22 14:25:01 +0500

    Update signup.dart

--- lib/Screens/signup.dart
+++ lib/Screens/signup.dart
@@ -5,7 +5,6 @@ import 'package:firebase_practice/Screens/login_screen.dart';
 import 'package:flutter/material.dart';
 
 import '../uitils/tostmessae.dart';
-
 class Signup extends StatefulWidget {
   
   Signup({super.key});
