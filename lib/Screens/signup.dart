adc9cdc485a50e4fe9b72787a7aca5925dfa6655
Ismailakram300 <123332697+Ismailakram300@users.noreply.github.com>
2025-07-22 14:24:36 +0500

    Update signup.dart

--- lib/Screens/signup.dart
+++ lib/Screens/signup.dart
@@ -3,7 +3,6 @@ import 'package:firebase_practice/Components/mybutton.dart';
 import 'package:firebase_practice/Components/mytextfield.dart';
 
 import 'package:firebase_practice/Screens/login_screen.dart';
-
 import 'package:flutter/material.dart';
 
 import '../uitils/tostmessae.dart';
